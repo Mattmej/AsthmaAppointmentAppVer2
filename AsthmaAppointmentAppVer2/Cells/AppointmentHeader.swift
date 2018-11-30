bfb6da30c0b04016013bccb982cbaea4ab462b76
Matt Mejia <matt@ADMINs-MacBook-Pro.local>
2018-11-29 19:39:46 -0500

    Add a header view with image and label, as well as a tableView, to the project.

---
+++ AsthmaAppointmentAppVer2/Cells/AppointmentHeader.swift
@@ -0,0 +1,9 @@
+//
+//  AppointmentHeader.swift
+//  AsthmaAppointmentAppVer2
+//
+//  Created by Matt Mejia on 11/29/18.
+//  Copyright © 2018 Matt_Mejia. All rights reserved.
+//
+
+import Foundation
---
+++ AsthmaAppointmentAppVer2/Cells/AppointmentTableViewCell.swift
@@ -0,0 +1,9 @@
+//
+//  AppointmentTableViewCell.swift
+//  AsthmaAppointmentAppVer2
+//
+//  Created by Matt Mejia on 11/29/18.
+//  Copyright © 2018 Matt_Mejia. All rights reserved.
+//
+
+import Foundation
