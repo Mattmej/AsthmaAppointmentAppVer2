954399d316f942532104da97d7935857620949bb
Matt Mejia <matt@ADMINs-MacBook-Pro.local>
2018-11-29 19:12:43 -0500

    Start a new project.

---
+++ AsthmaAppointmentAppVer2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AsthmaAppointmentAppVer2
+//
+//  Created by Matt Mejia on 11/29/18.
+//  Copyright © 2018 Matt_Mejia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
