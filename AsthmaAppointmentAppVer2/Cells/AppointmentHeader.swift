8a972f70cdf7f6698a990d9e368f471cbd145c9d
Matt Mejia <matt@ADMINs-MacBook-Pro.local>
2018-11-29 19:46:07 -0500

    Create header cell view.

--- AsthmaAppointmentAppVer2/Cells/AppointmentHeader.swift
+++ AsthmaAppointmentAppVer2/Cells/AppointmentHeader.swift
@@ -6,4 +6,16 @@
 //  Copyright © 2018 Matt_Mejia. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class AppointmentHeader: UITableViewHeaderFooterView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
--- AsthmaAppointmentAppVer2/Cells/AppointmentTableViewCell.swift
+++ AsthmaAppointmentAppVer2/Cells/AppointmentTableViewCell.swift
@@ -6,4 +6,19 @@
 //  Copyright © 2018 Matt_Mejia. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class AppointmentTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
